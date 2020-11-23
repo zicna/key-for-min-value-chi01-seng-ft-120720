1cb8c8d5402a6d38afb1144102d6a1dd1a713af3
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-23 04:42:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  min = 10000
+  name_hash.each do |name, value|
+    if (value < min)
+      min = value
+    end
+    
+  end
 
 end
